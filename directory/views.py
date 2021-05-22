9a332a0974846e503fc436f3f7bba9324b79ee2d
JohnF <johnfitz16@gmail.com>
2021-05-22 15:35:09 -0400

    started building in forms to add contacts and display in detail view

--- directory/views.py
+++ directory/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.http import HttpResponseRedirect
 
 from .models import Contact
+from .forms import ContactForm
 
 class ContactList(ListView):
     model = Contact
     context_object_name = 'all_contacts'
 
-# def index(request):
-#     return render(request, 'index.html')
+def contact_req(request):
+    submitted = False
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/contact/?submitted=True')
+        
+        else:
+            form = ContactForm()
+            if 'submitted' in request.GET:
+                submitted = True
+    
+    # really not sure about this part - should redirect to detailed view of contact
+    return render(request, 'directory/contact.html', {'form': form, 'contact_info': Contact.objects.all(), 'submitted': submitted})
+
 
