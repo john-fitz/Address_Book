96d0837b6b3eb3cc48b917d090be667b1e06d0f5
JohnF <johnfitz16@gmail.com>
2021-05-26 18:55:42 -0400

    further editing and throwing files into the system before testing

--- directions/static/JS/google_places.js
+++ directions/static/JS/google_places.js
@@ -15,7 +15,7 @@ function initAutocomplete() {
    document.getElementById('id-google-address-a'),
    {
        types: ['address'],
-       componentRestrictions: {'country': ['uk']},
+       componentRestrictions: {'country': ['us']},
    })
   
   autocomplete_a.addListener('place_changed', function(){
@@ -27,7 +27,7 @@ function initAutocomplete() {
    document.getElementById('id-google-address-b'),
    {
        types: ['address'],
-       componentRestrictions: {'country': ['uk']},
+       componentRestrictions: {'country': ['us']},
    })
   
   autocomplete_b.addListener('place_changed', function(){
