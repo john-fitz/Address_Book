69ec35e93905f2d95ac2d8c77ca6239a7fc28cc0
JohnF <johnfitz16@gmail.com>
2021-05-27 09:22:33 -0400

    adding html and updating directions/urls.py to display map

--- directions/urls.py
+++ directions/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-	path('route/', views.route, name="route"),
+	path('', views.route, name="route"),
 	path('map/', views.map, name="map"),
 	]
