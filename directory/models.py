e73c4b2f27078dfcc6a00879880b5097178d8937
JohnF <johnfitz16@gmail.com>
2021-06-01 12:00:47 -0400

    adding address field to contact model

--- directory/models.py
+++ directory/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from address.models import AddressField
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=20)
@@ -8,9 +9,11 @@ class Contact(models.Model):
     #can use a new field called phone-number-field that you need to install
     email = models.EmailField(blank=True, null=True)
     birthday = models.DateField('Birthday', blank=True, null=True)
+    address = AddressField(blank=True, null=True)
     notes = models.TextField('Notes', max_length=500, blank=True) 
     username = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
     self_contact = models.BooleanField(default=False)
+    
 
     def __str__(self):
         return (self.first_name + " " + self.last_name)
