85b4ffa35e5c5eb55ed27cc4a97714e2cad5e6fc
JohnF <johnfitz16@gmail.com>
2021-06-08 13:24:42 -0400

    adding user address information to be passed to contact detailview to be used in directions

--- directory/views.py
+++ directory/views.py
@@ -33,11 +33,18 @@ class UpdateContactView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class ContactView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = Contact
-    context_object_name = 'contact'
+    # context_object_name = 'contact'
 
     def test_func(self):
         obj = self.get_object()
         return obj.username == self.request.user
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['user_info'] = Contact.objects.filter(username=self.request.user).filter(self_contact=True).values()[0]
+        return context
 
 class AddContactView(CreateView):
     model = Contact
