2d219dcc8300c526e7d2096af45882bc9733ba1b
JohnF <johnfitz16@gmail.com>
2021-06-08 12:57:05 -0400

    cleaning up files

--- directory/models.py
+++ directory/models.py
@@ -6,11 +6,8 @@ from django.contrib.auth.models import User
 class Contact(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    # phone_number = models.DateTimeField('Last Updated') 
-    #can use a new field called phone-number-field that you need to install
     email = models.EmailField(blank=True, null=True)
     birthday = models.DateField('Birthday', blank=True, null=True)
-    # address = AddressField(blank=True, null=True)
     notes = models.TextField('Notes', max_length=500, blank=True) 
     username = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
     address_line1 = models.CharField(max_length = 50, null=True, blank=True)
