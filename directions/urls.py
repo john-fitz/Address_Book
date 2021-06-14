a09f7a63c53120a59881734f8856203635cf6cf9
JohnF <johnfitz16@gmail.com>
2021-06-14 14:47:23 -0400

    rewrote maps api to route directly and deleted old files

--- directions/urls.py
+++ directions/urls.py
@@ -5,7 +5,5 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-	# path('<str:start_address>/<str:end_address>', views.route, name="route"),
-	path('', views.route, name="route"),
-	path('map/', views.map, name="map"),
+	path('', views.map, name="map"),
 	]
