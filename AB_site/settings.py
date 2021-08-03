13fdfafd486112a087f93df1833ab7a9373a7a1c
JohnF <johnfitz16@gmail.com>
2021-08-03 11:09:51 -0400

    changing secret key

--- AB_site/settings.py
+++ AB_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0fw#ob@2zb_7oz(7yo)nls+vg@fm##oqd(6vb90#d9ing6x+_l'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 GOOGLE_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
 
